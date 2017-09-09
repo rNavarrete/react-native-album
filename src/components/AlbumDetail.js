ad041fa4c2b9974f7d3d7e03f4ab5810b4aee2b2
Rolando Navarrete <rolando@splice.com>
2017-09-08 18:29:09 -0700

    correct album details

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,35 +5,35 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyle } = styles;
 
   return (
-      <Card>
-        <CardSection>
-            <View>
-              <Image
-                style={styles.thumbnailStyle}
-                source={{ uri: thumbnail_image }}
-              />
-            </View>
-            <View style={styles.headerContentStyle}>
-              <Text>{title}</Text>
-              <Text>{artist}</Text>
-            </View>
-        </CardSection>
-      </Card>
-    );
-  };
-
+    <Card>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+};
 
 const styles = {
-    headerContentStyle: {
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    thumbnailStyle: {
-      width: 50,
-      height: 50
-    }
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
