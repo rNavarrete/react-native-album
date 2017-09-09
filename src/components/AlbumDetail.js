c0754293634fc01a6f8ab5a19304eef4a10e1301
Rolando Navarrete <rolando@splice.com>
2017-09-08 18:28:04 -0700

    commited w/ bug

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-    return (
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
+  return (
+      <Card>
+        <CardSection>
+            <View>
+              <Image
+                style={styles.thumbnailStyle}
+                source={{ uri: thumbnail_image }}
+              />
+            </View>
+            <View style={styles.headerContentStyle}>
+              <Text>{title}</Text>
+              <Text>{artist}</Text>
+            </View>
+        </CardSection>
+      </Card>
     );
+  };
+
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+      width: 50,
+      height: 50
+    }
 };
 
 export default AlbumDetail;
